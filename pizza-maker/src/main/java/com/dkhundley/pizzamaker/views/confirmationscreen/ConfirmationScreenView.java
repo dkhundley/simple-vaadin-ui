cebc44e8addd4aa1c4494a17c8a0e3a052919d99
David Hundley <david.k.hundley@gmail.com>
2025-02-13 19:39:23 -0600

    Cleaning up files for results screen view

--- pizza-maker/src/main/java/com/dkhundley/pizzamaker/views/confirmationscreen/ConfirmationScreenView.java
+++ pizza-maker/src/main/java/com/dkhundley/pizzamaker/views/confirmationscreen/ConfirmationScreenView.java
@@ -1,3 +1,4 @@
+// Importing all the special code we need to build our Vaadin UI
 package com.dkhundley.pizzamaker.views.confirmationscreen;
 
 import com.vaadin.flow.component.Composite;
@@ -7,145 +8,222 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Hr;
 import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.router.QueryParameters;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.OptionalParameter;
 
+import java.util.List;
+import java.util.Map;
+
+// Setting our page route (Note: if this was empty, it would be considered the root or "home" page. Naturally, this is not our home page!)
 @PageTitle("Confirmation Screen")
 @Route("confirmation-screen")
-public class ConfirmationScreenView extends Composite<VerticalLayout> {
+public class ConfirmationScreenView extends Composite<VerticalLayout> implements HasUrlParameter<String> {
 
+    // Constructing the view
     public ConfirmationScreenView() {
-        H1 h1 = new H1();
-        H3 h3 = new H3();
-        Hr hr = new Hr();
-        H3 h32 = new H3();
-        Details details = new Details();
-        Details details2 = new Details();
-        Details details3 = new Details();
-        Hr hr2 = new Hr();
-        H3 h33 = new H3();
-        HorizontalLayout layoutRow = new HorizontalLayout();
-        Paragraph textLarge = new Paragraph();
-        Paragraph textLarge2 = new Paragraph();
-        HorizontalLayout layoutRow2 = new HorizontalLayout();
-        Paragraph textLarge3 = new Paragraph();
-        Paragraph textLarge4 = new Paragraph();
-        HorizontalLayout layoutRow3 = new HorizontalLayout();
-        Paragraph textLarge5 = new Paragraph();
-        Paragraph textLarge6 = new Paragraph();
-        HorizontalLayout layoutRow4 = new HorizontalLayout();
-        Paragraph textLarge7 = new Paragraph();
-        Paragraph textLarge8 = new Paragraph();
-        Hr hr3 = new Hr();
-        Button buttonSecondary = new Button();
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
-        h1.setText("Order Confirmation");
-        h1.setWidth("max-content");
-        h3.setText("Your pizza is in the oven!");
-        h3.setWidth("max-content");
-        h32.setText("Order Details");
-        h32.setWidth("max-content");
-        details.setWidth("100%");
-        setDetailsSampleData(details);
-        details2.setWidth("100%");
-        setDetailsSampleData(details2);
-        details3.setWidth("100%");
-        setDetailsSampleData(details3);
-        h33.setText("Pricing Details");
-        h33.setWidth("max-content");
-        layoutRow.setWidthFull();
-        getContent().setFlexGrow(1.0, layoutRow);
-        layoutRow.addClassName(Gap.MEDIUM);
-        layoutRow.setWidth("100%");
-        layoutRow.getStyle().set("flex-grow", "1");
-        textLarge.setText("Subtotal:");
-        textLarge.getStyle().set("flex-grow", "1");
-        textLarge.setMinWidth("100px");
-        textLarge.getStyle().set("font-size", "var(--lumo-font-size-xl)");
-        textLarge2.setText(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
-        textLarge2.setWidth("100%");
-        textLarge2.getStyle().set("font-size", "var(--lumo-font-size-xl)");
-        layoutRow2.setWidthFull();
-        getContent().setFlexGrow(1.0, layoutRow2);
-        layoutRow2.addClassName(Gap.MEDIUM);
-        layoutRow2.setWidth("100%");
-        layoutRow2.getStyle().set("flex-grow", "1");
-        textLarge3.setText("Tax:");
-        textLarge3.getStyle().set("flex-grow", "1");
-        textLarge3.setMinWidth("100px");
-        textLarge3.getStyle().set("font-size", "var(--lumo-font-size-xl)");
-        textLarge4.setText(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
-        textLarge4.setWidth("100%");
-        textLarge4.getStyle().set("font-size", "var(--lumo-font-size-xl)");
-        layoutRow3.setWidthFull();
-        getContent().setFlexGrow(1.0, layoutRow3);
-        layoutRow3.addClassName(Gap.MEDIUM);
-        layoutRow3.setWidth("100%");
-        layoutRow3.getStyle().set("flex-grow", "1");
-        textLarge5.setText("Tip:");
-        textLarge5.getStyle().set("flex-grow", "1");
-        textLarge5.setMinWidth("100px");
-        textLarge5.getStyle().set("font-size", "var(--lumo-font-size-xl)");
-        textLarge6.setText(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
-        textLarge6.setWidth("100%");
-        textLarge6.getStyle().set("font-size", "var(--lumo-font-size-xl)");
-        layoutRow4.setWidthFull();
-        getContent().setFlexGrow(1.0, layoutRow4);
-        layoutRow4.addClassName(Gap.MEDIUM);
-        layoutRow4.setWidth("100%");
-        layoutRow4.getStyle().set("flex-grow", "1");
-        textLarge7.setText("TOTAL:");
-        textLarge7.getStyle().set("flex-grow", "1");
-        textLarge7.setMinWidth("100px");
-        textLarge7.getStyle().set("font-size", "var(--lumo-font-size-xl)");
-        textLarge8.setText(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
-        textLarge8.setWidth("100%");
-        textLarge8.getStyle().set("font-size", "var(--lumo-font-size-xl)");
-        buttonSecondary.setText("Order another pizza?");
-        buttonSecondary.setWidth("min-content");
-        getContent().add(h1);
-        getContent().add(h3);
-        getContent().add(hr);
-        getContent().add(h32);
-        getContent().add(details);
-        getContent().add(details2);
-        getContent().add(details3);
-        getContent().add(hr2);
-        getContent().add(h33);
-        getContent().add(layoutRow);
-        layoutRow.add(textLarge);
-        layoutRow.add(textLarge2);
-        getContent().add(layoutRow2);
-        layoutRow2.add(textLarge3);
-        layoutRow2.add(textLarge4);
-        getContent().add(layoutRow3);
-        layoutRow3.add(textLarge5);
-        layoutRow3.add(textLarge6);
-        getContent().add(layoutRow4);
-        layoutRow4.add(textLarge7);
-        layoutRow4.add(textLarge8);
-        getContent().add(hr3);
-        getContent().add(buttonSecondary);
     }
 
-    private void setDetailsSampleData(Details details) {
-        Span name = new Span("Sophia Williams");
-        Span email = new Span("sophia.williams@company.com");
-        Span phone = new Span("(501) 555-9128");
-        VerticalLayout content = new VerticalLayout(name, email, phone);
+    // #X. GETTING THE ORDER DETAILS FROM THE ORDER TAKER SCREEN!!!
+    // -------------------------------------------------------------------------------------------------------------------
+    // Instantiating variables to store order details
+    private String crustSize;
+    private List<String> meats;
+    private List<String> veggies;
+    private String tipPercentage;
+    private Float customTipAmount;
+    private Float crustSizeValue;
+    private List<Float> meatsValues;
+    private List<Float> veggiesValues;
+
+    // Setting the parameters from the pizza order taker view
+    @Override
+    public void setParameter(BeforeEvent event, @OptionalParameter String parameter) {
+        
+        // Extracting the values from the previous order taker view
+        QueryParameters queryParameters = event.getLocation().getQueryParameters();
+        Map<String, List<String>> parameterMap = queryParameters.getParameters();
+        
+        // Storing values in instance variables
+        crustSize = parameterMap.getOrDefault("crustSizeLabel", List.of("")).get(0);
+        meats = List.of(parameterMap.getOrDefault("meatsLabels", List.of("")).get(0).split(","));
+        veggies = List.of(parameterMap.getOrDefault("veggiesLabels", List.of("")).get(0).split(","));
+        tipPercentage = parameterMap.getOrDefault("tipPercentage", List.of("")).get(0);
+        
+        // Parse numeric values safely
+        String customTipStr = parameterMap.getOrDefault("customTipAmount", List.of("0")).get(0);
+        customTipAmount = customTipStr.isEmpty() ? 0f : Float.parseFloat(customTipStr);
+        
+        crustSizeValue = Float.parseFloat(parameterMap.getOrDefault("crustSizeValue", List.of("0")).get(0));
+        
+        // Parse arrays of float values
+        String meatsValuesStr = parameterMap.getOrDefault("meatsValues", List.of("")).get(0);
+        meatsValues = meatsValuesStr.isEmpty() ? 
+            List.of() : 
+            List.of(meatsValuesStr.split(",")).stream()
+                .map(Float::parseFloat)
+                .toList();
+        
+        String veggiesValuesStr = parameterMap.getOrDefault("veggiesValues", List.of("")).get(0);
+        veggiesValues = veggiesValuesStr.isEmpty() ? 
+            List.of() : 
+            List.of(veggiesValuesStr.split(",")).stream()
+                .map(Float::parseFloat)
+                .toList();
+
+        // Initializing the UI (See below!)
+        initializeUI();
+    }
+
+    // #X. DEFINING THE VIEW OF THE ORDER RESULTS SCREEN
+    // -------------------------------------------------------------------------------------------------------------------
+    private void initializeUI() {
+
+        // Page title and subtitle
+        H1 title = new H1("Order Confirmation");
+        H3 subtitle = new H3("Your pizza is in the oven!");
+
+        // Order details section
+        H3 orderDetailsHeader = new H3("Order Details");
+        Details crustSizeDetails = createCrustDetailsSection();
+        Details meatDetails = createMeatDetailsSection();
+        Details veggieDetails = createVeggieDetailsSection();
+
+        // Pricing details section
+        H3 pricingDetailsHeader = new H3("Pricing Details");
+        HorizontalLayout subtotalRow = createPricingRow("Subtotal:", "$0.00");
+        HorizontalLayout taxRow = createPricingRow("Tax:", "$0.00");
+        HorizontalLayout tipRow = createTipRow("Tip:", "$0.00");
+        HorizontalLayout totalRow = createPricingRow("TOTAL:", "$0.00");
+
+        // Secondary action button
+        Button orderAnotherPizzaButton = new Button("Order another pizza?");
+
+        // Adding components to the overall layout
+        getContent().add(
+            title, 
+            subtitle, 
+            new Hr(), 
+            orderDetailsHeader, 
+            crustSizeDetails, 
+            meatDetails, 
+            veggieDetails, 
+            new Hr(), 
+            pricingDetailsHeader, 
+            subtotalRow, 
+            taxRow, 
+            tipRow, 
+            totalRow, 
+            new Hr(), 
+            orderAnotherPizzaButton
+        );
+    }
+
+    private HorizontalLayout createPricingRow(String label, String value) {
+        HorizontalLayout row = new HorizontalLayout();
+        row.setWidthFull();
+        row.addClassName(Gap.MEDIUM);
+        row.getStyle().set("flex-grow", "1");
+
+        Paragraph labelParagraph = new Paragraph(label);
+        labelParagraph.getStyle().set("flex-grow", "1");
+        labelParagraph.setMinWidth("100px");
+        labelParagraph.getStyle().set("font-size", "var(--lumo-font-size-xl)");
+
+        Paragraph valueParagraph = new Paragraph(value);
+        valueParagraph.setWidth("100%");
+        valueParagraph.getStyle().set("font-size", "var(--lumo-font-size-xl)");
+
+        row.add(labelParagraph, valueParagraph);
+        return row;
+    }
+
+    private HorizontalLayout createTipRow(String label, String value) {
+        HorizontalLayout row = new HorizontalLayout();
+        row.setWidthFull();
+        row.addClassName(Gap.MEDIUM);
+        row.getStyle().set("flex-grow", "1");
+
+        Paragraph labelParagraph = new Paragraph(label);
+        labelParagraph.getStyle().set("flex-grow", "1");
+        labelParagraph.setMinWidth("100px");
+        labelParagraph.getStyle().set("font-size", "var(--lumo-font-size-xl)");
+
+        Paragraph valueParagraph = new Paragraph(value);
+        valueParagraph.setWidth("100%");
+        valueParagraph.getStyle().set("font-size", "var(--lumo-font-size-xl)");
+
+        row.add(labelParagraph, valueParagraph);
+        return row;
+    }
+
+
+
+    // SETTING CRUST DETAILS
+    // -------------------------------------------------------------------------------------------------------------------
+    private void setDetailsCrustSize(Details details) {
+        Span crustSizeSpan = new Span(this.crustSize);
+        VerticalLayout content = new VerticalLayout(crustSizeSpan);
         content.setSpacing(false);
         content.setPadding(false);
-        details.setSummaryText("Contact information");
-        details.setOpened(true);
+        details.setSummaryText("Crust Size");
+        details.add(content);
         details.setContent(content);
+    }
+
+    private Details createCrustDetailsSection() {
+        Details details = new Details();
+        details.setWidth("100%");
+        setDetailsCrustSize(details);
+        return details;
+    }
+
+    // SETTING MEAT TOPPING DETAILS
+    // -------------------------------------------------------------------------------------------------------------------
+    private void setDetailsMeats(Details details) {
+        VerticalLayout content = new VerticalLayout();
+        this.meats.forEach(meat -> {
+            Span meatSpan = new Span(meat);
+            content.add(meatSpan);
+        });
+        details.setSummaryText("Meat Toppings");
+        details.setOpened(false);
+        details.setContent(content);
+    }
+
+    private Details createMeatDetailsSection() {
+        Details details = new Details();
+        details.setWidth("100%");
+        setDetailsMeats(details);
+        return details;
+    }
+
+    // SETTING VEGGIE TOPPING DETAILS
+    // -------------------------------------------------------------------------------------------------------------------
+    private void setDetailsVeggies(Details details) {
+        VerticalLayout content = new VerticalLayout();
+        this.veggies.forEach(veggie -> {
+            Span veggieSpan = new Span(veggie);
+            content.add(veggieSpan);
+        });
+        details.setSummaryText("Veggie Toppings");
+        details.setOpened(false);
+        details.setContent(content);
+    }
+
+    private Details createVeggieDetailsSection() {
+        Details details = new Details();
+        details.setWidth("100%");
+        setDetailsVeggies(details);
+        return details;
     }
 }
--- pizza-maker/src/main/java/com/dkhundley/pizzamaker/views/ordertaker/OrderTakerView.java
+++ pizza-maker/src/main/java/com/dkhundley/pizzamaker/views/ordertaker/OrderTakerView.java
@@ -1,3 +1,4 @@
+// Importing all the special code we need to build our Vaadin UI
 package com.dkhundley.pizzamaker.views.ordertaker;
 
 import com.vaadin.flow.component.Composite;
@@ -19,92 +20,175 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import java.util.ArrayList;
 import java.util.List;
+import com.vaadin.flow.component.UI;
+import com.dkhundley.pizzamaker.views.confirmationscreen.ConfirmationScreenView;
+import com.vaadin.flow.router.QueryParameters;
+import java.util.Map;
 
 @PageTitle("Order Taker")
 @Route("")
 public class OrderTakerView extends Composite<VerticalLayout> {
 
     public OrderTakerView() {
-        H1 h1 = new H1();
-        H2 h2 = new H2();
-        Hr hr = new Hr();
-        H3 h3 = new H3();
-        FormLayout formLayout3Col = new FormLayout();
-        Select select = new Select();
-        MultiSelectComboBox multiSelectComboBox = new MultiSelectComboBox();
-        MultiSelectComboBox multiSelectComboBox2 = new MultiSelectComboBox();
-        Hr hr2 = new Hr();
-        H3 h32 = new H3();
-        RadioButtonGroup radioGroup = new RadioButtonGroup();
-        NumberField numberField = new NumberField();
-        Button buttonPrimary = new Button();
+        // Page title
+        H1 title = new H1("Pizza Maker");
+        title.setWidth("max-content");
+
+        // Subtitle
+        H2 subtitle = new H2("Create your pizza below!");
+        subtitle.setWidth("max-content");
+
+        // Horizontal line separator
+        Hr separator1 = new Hr();
+
+        // Toppings section title
+        H3 toppingsTitle = new H3("Select Your Toppings");
+        toppingsTitle.setWidth("max-content");
+
+        // Form layout for pizza options
+        FormLayout pizzaOptionsForm = new FormLayout();
+        pizzaOptionsForm.setWidth("100%");
+        pizzaOptionsForm.setResponsiveSteps(
+            new ResponsiveStep("0", 1),
+            new ResponsiveStep("250px", 2),
+            new ResponsiveStep("500px", 3)
+        );
+
+        // Crust size selection
+        Select<SampleItem> crustSizeSelect = new Select<>();
+        crustSizeSelect.setLabel("Crust Size");
+        crustSizeSelect.setWidth("min-content");
+        setSelectSampleData(crustSizeSelect);
+
+        // Meats selection
+        MultiSelectComboBox<SampleItem> meatsSelect = new MultiSelectComboBox<>();
+        meatsSelect.setLabel("Meats");
+        meatsSelect.setWidth("min-content");
+        setMeatsSelectSampleData(meatsSelect);
+
+        // Veggies selection
+        MultiSelectComboBox<SampleItem> veggiesSelect = new MultiSelectComboBox<>();
+        veggiesSelect.setLabel("Veggies");
+        veggiesSelect.setWidth("min-content");
+        setVeggiesSelectSampleData(veggiesSelect);
+
+        // Horizontal line separator
+        Hr separator2 = new Hr();
+
+        // Tip section title
+        H3 tipTitle = new H3("Add a Tip?");
+        tipTitle.setWidth("max-content");
+
+        // Tip percentage radio buttons
+        RadioButtonGroup<String> tipPercentageRadioGroup = new RadioButtonGroup<>();
+        tipPercentageRadioGroup.setLabel("Percentage");
+        tipPercentageRadioGroup.setWidth("min-content");
+        tipPercentageRadioGroup.setItems("10%", "15%", "20%", "Custom");
+        tipPercentageRadioGroup.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
+
+        // Custom tip amount field
+        NumberField customTipAmountField = new NumberField("Custom Amount");
+        customTipAmountField.setWidth("min-content");
+        customTipAmountField.setVisible(false);
+        tipPercentageRadioGroup.addValueChangeListener(event -> {
+            String value = event.getValue();
+            customTipAmountField.setVisible("Custom".equals(value));
+        });
+
+        // Submit order button
+        Button submitOrderButton = new Button("Submit Order!", event -> {
+            boolean isValid = true;
+            String errorMessage = "";
+
+            // Ensuring that all the appropriate fields have been filled in
+            if (crustSizeSelect.getValue() == null) {
+                isValid = false;
+                errorMessage += "Please select a crust size.\n";
+            }
+            if (meatsSelect.getValue().isEmpty()) {
+                isValid = false;
+                errorMessage += "Please select at least one meat topping.\n";
+            }
+            if (veggiesSelect.getValue().isEmpty()) {
+                isValid = false;
+                errorMessage += "Please select at least one veggie topping.\n";
+            }
+            if (tipPercentageRadioGroup.getValue() == null) {
+                isValid = false;
+                errorMessage += "Please select a tip percentage.\n";
+            } else if ("Custom".equals(tipPercentageRadioGroup.getValue()) && 
+                      (customTipAmountField.getValue() == null || customTipAmountField.getValue() <= 0)) {
+                isValid = false;
+                errorMessage += "Please enter a valid custom tip amount.\n";
+            }
+
+            // Submitting the pizza order
+            if (isValid) {
+                QueryParameters params = QueryParameters.simple(Map.of(
+                    "crustSizeValue", crustSizeSelect.getValue().value().toString(),
+                    "crustSizeLabel", crustSizeSelect.getValue().label(),
+                    "meatsValues", String.join(",", meatsSelect.getValue().stream().map(item -> item.value().toString()).toList()),
+                    "meatsLabels", String.join(",", meatsSelect.getValue().stream().map(SampleItem::label).toList()),
+                    "veggiesValues", String.join(",", veggiesSelect.getValue().stream().map(item -> item.value().toString()).toList()),
+                    "veggiesLabels", String.join(",", veggiesSelect.getValue().stream().map(SampleItem::label).toList()),
+                    "tipPercentage", tipPercentageRadioGroup.getValue(),
+                    "customTipAmount", customTipAmountField.getValue() != null ? 
+                        customTipAmountField.getValue().toString() : ""
+                ));
+                UI.getCurrent().navigate(ConfirmationScreenView.class, params);
+            } else {
+                com.vaadin.flow.component.notification.Notification.show(
+                    errorMessage,
+                    3000,
+                    com.vaadin.flow.component.notification.Notification.Position.MIDDLE
+                );
+            }
+        });
+        submitOrderButton.setWidth("min-content");
+        submitOrderButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        // Add components to the layout
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
-        h1.setText("Pizza Maker");
-        h1.setWidth("max-content");
-        h2.setText("Create your pizza below!");
-        h2.setWidth("max-content");
-        h3.setText("Select Your Toppings");
-        h3.setWidth("max-content");
-        formLayout3Col.setWidth("100%");
-        formLayout3Col.setResponsiveSteps(new ResponsiveStep("0", 1), new ResponsiveStep("250px", 2),
-                new ResponsiveStep("500px", 3));
-        select.setLabel("Crust Size");
-        select.setWidth("min-content");
-        setSelectSampleData(select);
-        multiSelectComboBox.setLabel("Meats");
-        multiSelectComboBox.setWidth("min-content");
-        setMultiSelectComboBoxSampleData(multiSelectComboBox);
-        multiSelectComboBox2.setLabel("Veggies");
-        multiSelectComboBox2.setWidth("min-content");
-        setMultiSelectComboBoxSampleData(multiSelectComboBox2);
-        h32.setText("Add a Tip?");
-        h32.setWidth("max-content");
-        radioGroup.setLabel("Percentage");
-        radioGroup.setWidth("min-content");
-        radioGroup.setItems("Order ID", "Product Name", "Customer", "Status");
-        radioGroup.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
-        numberField.setLabel("Custom Amount");
-        numberField.setWidth("min-content");
-        buttonPrimary.setText("Submit Order!");
-        buttonPrimary.setWidth("min-content");
-        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        getContent().add(h1);
-        getContent().add(h2);
-        getContent().add(hr);
-        getContent().add(h3);
-        getContent().add(formLayout3Col);
-        formLayout3Col.add(select);
-        formLayout3Col.add(multiSelectComboBox);
-        formLayout3Col.add(multiSelectComboBox2);
-        getContent().add(hr2);
-        getContent().add(h32);
-        getContent().add(radioGroup);
-        getContent().add(numberField);
-        getContent().add(buttonPrimary);
+        getContent().add(title, subtitle, separator1, toppingsTitle, pizzaOptionsForm, separator2, tipTitle, tipPercentageRadioGroup, customTipAmountField, submitOrderButton);
+        pizzaOptionsForm.add(crustSizeSelect, meatsSelect, veggiesSelect);
     }
 
-    record SampleItem(String value, String label, Boolean disabled) {
+    // Sample item record
+    record SampleItem(Float value, String label, Boolean disabled) {
     }
 
-    private void setSelectSampleData(Select select) {
+    // Set sample data for Select component
+    private void setSelectSampleData(Select<SampleItem> select) {
         List<SampleItem> sampleItems = new ArrayList<>();
-        sampleItems.add(new SampleItem("first", "First", null));
-        sampleItems.add(new SampleItem("second", "Second", null));
-        sampleItems.add(new SampleItem("third", "Third", Boolean.TRUE));
-        sampleItems.add(new SampleItem("fourth", "Fourth", null));
+        sampleItems.add(new SampleItem(8.00f, "Small ($8.00)", null));
+        sampleItems.add(new SampleItem(10.00f, "Medium ($10.00)", null));
+        sampleItems.add(new SampleItem(12.00f, "Large ($12.00)", null));
+        sampleItems.add(new SampleItem(14.00f, "Extra Large ($14.00)", null));
         select.setItems(sampleItems);
-        select.setItemLabelGenerator(item -> ((SampleItem) item).label());
-        select.setItemEnabledProvider(item -> !Boolean.TRUE.equals(((SampleItem) item).disabled()));
+        select.setItemLabelGenerator(SampleItem::label);
+        select.setItemEnabledProvider(item -> !Boolean.TRUE.equals(item.disabled()));
     }
 
-    private void setMultiSelectComboBoxSampleData(MultiSelectComboBox multiSelectComboBox) {
+    // Set sample data for Meats MultiSelectComboBox component
+    private void setMeatsSelectSampleData(MultiSelectComboBox<SampleItem> multiSelectComboBox) {
         List<SampleItem> sampleItems = new ArrayList<>();
-        sampleItems.add(new SampleItem("first", "First", null));
-        sampleItems.add(new SampleItem("second", "Second", null));
-        sampleItems.add(new SampleItem("third", "Third", Boolean.TRUE));
-        sampleItems.add(new SampleItem("fourth", "Fourth", null));
+        sampleItems.add(new SampleItem(1.50f, "Pepperoni ($1.50)", null));
+        sampleItems.add(new SampleItem(1.50f, "Sausage ($1.50)", null));
+        sampleItems.add(new SampleItem(1.75f, "Bacon ($1.75)", null));
+        sampleItems.add(new SampleItem(1.50f, "Ham ($1.50)", null));
         multiSelectComboBox.setItems(sampleItems);
-        multiSelectComboBox.setItemLabelGenerator(item -> ((SampleItem) item).label());
+        multiSelectComboBox.setItemLabelGenerator(SampleItem::label);
+    }
+
+    // Set sample data for Veggies MultiSelectComboBox component
+    private void setVeggiesSelectSampleData(MultiSelectComboBox<SampleItem> multiSelectComboBox) {
+        List<SampleItem> sampleItems = new ArrayList<>();
+        sampleItems.add(new SampleItem(1.00f, "Mushrooms ($1.00)", null));
+        sampleItems.add(new SampleItem(0.75f, "Onions ($0.75)", null));
+        sampleItems.add(new SampleItem(0.75f, "Peppers ($0.75)", null));
+        sampleItems.add(new SampleItem(1.00f, "Olives ($1.00)", null));
+        multiSelectComboBox.setItems(sampleItems);
+        multiSelectComboBox.setItemLabelGenerator(SampleItem::label);
     }
 }
